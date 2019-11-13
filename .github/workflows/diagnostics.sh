fa047bfa009fb96cf82329d1c946efd0c3bd3d7b
Dave Syer <dsyer@pivotal.io>
2019-11-13 08:46:52 +0000

    Rename run/cleanup scripts again

--- .github/workflows/diagnostics.sh
+++ .github/workflows/diagnostics.sh
@@ -4,6 +4,11 @@ set -o nounset
 
 fats_dir=`dirname "${BASH_SOURCE[0]}"`/fats
 
+echo "##[group]Docker daemon"
+cat /etc/docker/daemon.json
+echo "##[endgroup]"
+
+
 if [ -d "$fats_dir" ]; then
   source $fats_dir/diagnostics.sh
 fi
--- .github/workflows/fats.sh
+++ .github/workflows/fats.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -16,7 +18,7 @@ readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 # fetch FATS scripts
 fats_dir=`dirname "${BASH_SOURCE[0]}"`/fats
 fats_repo="dsyer/fats"
-fats_refspec=master # master as of 2019-11-07
+fats_refspec=fac7395 # master as of 2019-11-07
 source `dirname "${BASH_SOURCE[0]}"`/fats-fetch.sh $fats_dir $fats_refspec $fats_repo
 source $fats_dir/.util.sh
 
@@ -28,6 +30,6 @@ kubectl create namespace $NAMESPACE
 fats_create_push_credentials $NAMESPACE
 
 # run test functions
-`dirname "${BASH_SOURCE[0]}"`/run.sh
+`dirname "${BASH_SOURCE[0]}"`/tests-run.sh
 # clean up
-`dirname "${BASH_SOURCE[0]}"`/cleanup.sh
+`dirname "${BASH_SOURCE[0]}"`/tests-cleanup.sh
