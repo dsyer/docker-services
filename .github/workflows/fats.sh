ba85b34fd23fe773bc0eecd05ff11a8d3c1068e6
Dave Syer <dsyer@pivotal.io>
2019-11-13 13:05:49 +0000

    Adjust to changes in fats

--- .github/workflows/fats.sh
+++ .github/workflows/fats.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -18,7 +16,7 @@ readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 # fetch FATS scripts
 fats_dir=`dirname "${BASH_SOURCE[0]}"`/fats
 fats_repo="dsyer/fats"
-fats_refspec=2a687ef # master as of 2019-11-07
+fats_refspec=4bc38a1 # master as of 2019-11-07
 source `dirname "${BASH_SOURCE[0]}"`/fats-fetch.sh $fats_dir $fats_repo $fats_refspec
 source $fats_dir/.util.sh
 
@@ -28,8 +26,3 @@ source $fats_dir/start.sh
 # setup namespace
 kubectl create namespace $NAMESPACE
 fats_create_push_credentials $NAMESPACE
-
-# run test functions
-`dirname "${BASH_SOURCE[0]}"`/tests-run.sh
-# clean up
-`dirname "${BASH_SOURCE[0]}"`/tests-cleanup.sh
